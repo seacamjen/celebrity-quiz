f54f758d0ec7a461f12861cadf7cf3f9788de8f7
cameron jensen and jahan walsh <cameron@>
2017-02-16 14:18:47 -0800

    Add if statement to assign celebrity according to color choice.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,26 @@
 $(function(){
 
   $(".ownInfo").submit(function(){
-    var person1 = $("input#firstname").val();
+    var person1 = $("input#firstName").val();
     var person2 =
-    $("input#lastname").val();
+    $("input#lastName").val();
 
-    $(".firstname").text("person1");
-    $(".lastname").text("person2");
+    $(".firstname").text(person1);
+    $(".lastname").text(person2);
 
     var color = $("input:radio[name=color]:checked").val();
 
     var food = $("#food").val();
 
+    if (color === 'blue'){
+      color = 'Travolta'
+    } else if ( color === 'red') {
+      color = 'Cage'
+    } else {
+      color = 'Damon'
+    }
+
+    $("#celebrity").empty().append(color);
     $(".panel").show();
     event.preventDefault();
   });
